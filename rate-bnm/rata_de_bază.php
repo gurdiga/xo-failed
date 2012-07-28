302eb223554ecfdda3f2fb5de4ae0554b318d84d
Vlad GURDIGA <gurdiga@gmail.com>
2012-07-28 13:36:23 +0300

    cîrpit rate-bnm/rata_de_bază.php

--- rate-bnm/rata_de_bază.php
+++ rate-bnm/rata_de_bază.php
@@ -20,7 +20,7 @@ $data = date('Y-m-d', mktime(0, 0, 0, $data['month'], $data['day'], $data['year'
 $root = dirname(__FILE__);
 $fişier = "$root/rata_de_bază.js";
 
-$rate = json_decode(file_get_contents($json), true);
+$rate = json_decode(file_get_contents($fişier), true);
 $rate[$data] = $valoarea;
 
 file_put_contents($fişier, 'var RateDeBază = ' . json_encode($rate) . ';');
