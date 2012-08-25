b5e8a3675ac00454e3e9d50b4a20aa45b1edd3cd
Vlad GURDIGA <gurdiga@gmail.com>
2012-08-25 13:03:44 +0200

    transmite datele din profil în rapoarte

--- js/raport.js
+++ js/raport.js
@@ -2,7 +2,7 @@ window.onload = function() {
   var nume = decodeURIComponent(location.pathname.match(/\/([^\/]+).html$/)[1]),
       context = {
         procedură: top.opener.Formular.colectează(),
-        executor: {}
+        executor: top.opener.Profil.date
       };
 
   top.opener.compile(context, this.document);
