71dab6e67859631bb2624df780dc9f34d8c4a522
Vlad GURDIGA <gurdiga@gmail.com>
2012-09-08 11:18:29 +0200

    îmbunătăţit rapopartele
    - creat /css/shared.css
    - refactorit raport.js
    - adăugat bara de instrumente şi buton de închidere
    - implementat butonul de imprimare

--- js/raport.js
+++ js/raport.js
@@ -1,23 +1,66 @@
-window.onload = function() {
-  var nume = decodeURIComponent(location.pathname.match(/\/([^\/]+).html$/)[1]),
-      pagina = decodeURIComponent(location.pathname),
-      context = {
-        procedură: opener.Formular.colectează(),
-        executor: opener.Profil.date,
-        moment: opener.moment,
-        login: opener.Utilizator.login,
-        $el: opener.Rapoarte[pagina].$el,
-        nume: function(persoană) {
-          return persoană['gen-persoană'] == 'fizică' ? persoană['nume'] : persoană['denumire'];
-        },
-        id: function(persoană) {
-          return persoană['gen-persoană'] == 'fizică' ? ('IDNP ' + persoană['idnp']) : ('IDNO ' + persoană['idno']);
-        }
-      };
+var Raport = {
+  init: function() {
+    Raport.$ = opener.$(document.body);
+    Raport.pagina = decodeURIComponent(location.pathname);
 
-  if (window.init) window.init(context);
+    Raport.compilează();
+    Raport.butonDeÎnchidere.init();
+    Raport.baraDeInstrumente.init();
 
-  opener.compile(context, this.document);
-  opener.$(this.document.body).find('.editabil').attr('contenteditable', true);
-  //history.replaceState(null, null, '/' + opener.Utilizator.login + context.procedură.număr + '/text-personalizabil-text-personalizabil');
+    Raport.$.find('.editabil').attr('contenteditable', true);
+
+    //var originalLocation = location.href;
+    //history.replaceState(null, null, '/' + opener.Utilizator.login + context.procedură.număr + '/text-personalizabil-text-personalizabil');
+  },
+
+  compilează: function() {
+    var context = {
+      procedură: opener.Formular.colectează(),
+      executor: opener.Profil.date,
+      moment: opener.moment,
+      login: opener.Utilizator.login,
+      $el: opener.Rapoarte[Raport.pagina].$el,
+      nume: function(persoană) {
+        return persoană['gen-persoană'] == 'fizică' ? persoană['nume'] : persoană['denumire'];
+      },
+      id: function(persoană) {
+        return persoană['gen-persoană'] == 'fizică' ? ('IDNP ' + persoană['idnp']) : ('IDNO ' + persoană['idno']);
+      }
+    };
+
+    if (window.init) window.init(context);
+
+    opener.compile(context, document);
+  },
+
+  baraDeInstrumente: {
+    init: function() {
+      opener.$şabloane.find('.bara-de-instrumente.pentru.raport').clone()
+        .appendTo(Raport.$)
+        .on('click', '.salvează', Raport.salvează)
+        .on('click', '.imprimă', Raport.imprimă)
+    }
+  },
+
+  salvează: function() {
+    console.log('TODO: post');
+  },
+
+  imprimă: function() {
+    window.print();
+  },
+
+  butonDeÎnchidere: {
+    init: function() {
+      opener.Formular.$.find('button.închide').clone()
+        .appendTo(Raport.$)
+        .on('click', Raport.închide);
+    }
+  },
+
+  închide: function() {
+    opener.Rapoarte[Raport.pagina].tab.close();
+  }
 };
+
+window.onload = Raport.init;
