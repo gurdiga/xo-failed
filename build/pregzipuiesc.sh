d51f737a0e280a397e77d275affac3e9d80eb277
Vlad GURDIGA <gurdiga@gmail.com>
2012-09-25 07:06:38 +0300

    Pregzipuiesc date...

--- build/pregzipuiesc.sh
+++ build/pregzipuiesc.sh
@@ -9,11 +9,20 @@ files="
   js/raport.js
 "
 
-echo "Pregzipuiesc..."
+echo "Pregzipuiesc cod..."
 
 for file in $files; do
   echo "- $file"
   cat $file | gzip > $file.gz
 done
+
+
+echo "Pregzipuiesc date..."
+
+for file in `find date -type f ! -name '*.gz'`; do
+  echo "- $file"
+  gzip $file
+done
+
 
 echo ''
