73b1a9025f7638e690bf0ae7ca0dc7c1b193b12e
Vlad GURDIGA <gurdiga@gmail.com>
2012-11-04 07:14:03 +0200

    mai mutat nişte funcţii în lib (pregătesc pentru DELETE)

--- bin/notează-ca-recentă.php
+++ bin/notează-ca-recentă.php
@@ -5,20 +5,9 @@ require_once './lib.php';
 
 $login = $_SERVER['PHP_AUTH_USER'];
 $procedură = file_get_contents('php://input');
+$afişează = true;
 
 verifică_login($login);
 verifică_număr($procedură);
 verifică_dacă_există($procedură);
-notează_ca_recentă($procedură, true);
-
-// ==============================
-
-function verifică_dacă_există($procedură) {
-  global $login;
-
-  $cale = "../date/$login/proceduri/$procedură.json.gz";
-
-  if (!file_exists($cale)) {
-    stop("Nu există procedura: [$cale]");
-  }
-}
+notează_ca_recentă($procedură, $afişează);
