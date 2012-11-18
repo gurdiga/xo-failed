27f7047e59442469961dd0c8134ba699ea01cfa3
Vlad GURDIGA <gurdiga@gmail.com>
2012-11-18 17:02:25 +0200

    fix intests

--- build/teste/integrare/pregăteşte-utilizator.sh
+++ build/teste/integrare/pregăteşte-utilizator.sh
@@ -5,3 +5,5 @@ echo 'Cont utilizator de test...'
 php $DOCUMENT_ROOT/bin/htusers.php $LOGIN $PASSWORD >> $DOCUMENT_ROOT/.htusers
 grep "$LOGIN:" $DOCUMENT_ROOT/.htusers &> /dev/null
 verifică 'creat'
+
+cat $DOCUMENT_ROOT/.htusers
