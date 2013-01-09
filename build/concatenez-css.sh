3c82a30707d45bbe85f0132f5c4f677d77a44ee3
Vlad GURDIGA <gurdiga@gmail.com>
2013-01-09 02:56:02 -0500

    build: bibi progress sript concatenare CSS

--- build/concatenez-css.sh
+++ build/concatenez-css.sh
@@ -1,4 +1,4 @@
-echo -n 'Concatenez CSS'
+echo -n 'Concatenez CSS.'
 
 TIMESTAMP=`date +'%F-%H-%m-%M'`
 
@@ -10,12 +10,16 @@ cat \
 
 mv css/one.css css/style-$TIMESTAMP.css
 
+echo -n '.'
+
 grep --fixed-strings --invert-match \
   -e '<link type="text/css" rel="stylesheet" href="/css/jquery-ui-1.9.2.custom.min.css"/>' \
   -e '<link type="text/css" rel="stylesheet" href="/css/shared.css"/' \
   index.html > index.html.1
 
 mv index.html.1 index.html
+
+echo -n '.'
 
 sed -i "s/style.css/style-$TIMESTAMP.css/g" index.html
 
