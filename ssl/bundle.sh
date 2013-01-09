bb84d4f7b7535f3193cd7a69182c5e8e000dd5cb
Vlad GURDIGA <gurdiga@gmail.com>
2013-01-08 23:35:48 -0500

    removed the scripts bundling SSL things
    Just put them in their final form in the GDrive Tehnic/ssl/ready-to-use
    folder.

--- ssl/bundle.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-
-unzip executori_org.zip
-cp executori_org.csr executori_org.csr.bak
-cat executori_org.csr.bak PositiveSSLCA2.crt AddTrustExternalCARoot.crt > executori_org.csr
