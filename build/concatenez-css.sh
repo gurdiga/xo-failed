a4ffe7ad71eee95975d7bd1676c0db1ebfcbc3c3
Vlad GURDIGA <gurdiga@gmail.com>
2013-01-09 02:45:45 -0500

    ajustat poziţie CSS $UI

--- build/concatenez-css.sh
+++ build/concatenez-css.sh
@@ -3,15 +3,15 @@ echo -n 'Concatenez CSS'
 TIMESTAMP=`date +'%F-%H-%m-%M'`
 
 cat \
+  css/jquery-ui-1.9.2.custom.min.css \
   css/shared.css \
   css/style.css \
-  lib/jquery-ui-1.9.2.custom/css/smoothness/jquery-ui-1.9.2.custom.min.css \
   > css/one.css
 
 mv css/one.css css/style-$TIMESTAMP.css
 
 grep --fixed-strings --invert-match \
-  -e '<link type="text/css" rel="stylesheet" href="/lib/jquery-ui-1.9.2.custom/css/smoothness/jquery-ui-1.9.2.custom.min.css"/>' \
+  -e '<link type="text/css" rel="stylesheet" href="/css/jquery-ui-1.9.2.custom.min.css"/>' \
   -e '<link type="text/css" rel="stylesheet" href="/css/shared.css"/' \
   index.html > index.html.1
 
