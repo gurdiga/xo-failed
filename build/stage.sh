8f55a4f2b3d4283aa5b4a37fbee5336adce464f1
Vlad GURDIGA <gurdiga@gmail.com>
2013-01-26 15:11:04 +0200

    build/stage: continuare

--- build/stage.sh
+++ build/stage.sh
@@ -43,11 +43,10 @@ fi
 STAGE_NGINX_CONF_D=/etc/nginx/conf.d/stage.executori.org
 sudo ln -f -s -T $ROOT/stage/nginx.conf.d $STAGE_NGINX_CONF_D
 cd stage
-echo SERVER_NAME=$SERVER_NAME ROOT=$ROOT ENV=stage build/start.sh
-SERVER_NAME=$SERVER_NAME ROOT=$ROOT ENV=stage build/start.sh
+SERVER_NAME=stage.executori.org ROOT=$ROOT ENV=stage build/start.sh
 sudo unlink $STAGE_NGINX_CONF_D
 
-SERVER_NAME=$SERVER_NAME ROOT=$ROOT ENV=prod NORESTART=1 build/configurez-nginx.sh
+SERVER_NAME=executori.org ROOT=$ROOT ENV=prod NORESTART=1 build/configurez-nginx.sh
 sudo ln -f -s -T $ROOT/prod/nginx.conf.d /etc/nginx/conf.d/executori.org
 
 cd ..
