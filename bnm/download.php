7fd87ec0cd32a3a66c2ce7b044e5fba500e8cb9d
Vlad GURDIGA <gurdiga@gmail.com>
2013-01-26 07:26:30 +0200

    bnm/download.php: log + bibi

--- bnm/download.php
+++ bnm/download.php
@@ -1,24 +1,24 @@
 <?
 
+$root = dirname(__FILE__);
+
 ini_set('display_errors', 1);
+ini_set('error_log', "$root/" . basename(__FILE__, '.php') . '.log');
 date_default_timezone_set('Europe/Chisinau');
+error_log('Start');
 
 $data = date('d.m.Y');
 $url = "http://bnm.md/md/official_exchange_rates?get_xml=1&date=$data";
 
-$root = dirname(__FILE__);
 $dir = "$root/" . date('Y/m/');
-$xml_file = $dir . date('d') . '.xml';
-$json_file = $dir . date('d') . '.js';
+$js = $dir . date('d') . '.js';
 
-if (file_exists($xml_file)) exit;
+if (file_exists($js)) exit;
 
 if (!is_dir($dir)) mkdir($dir, 0755, true);
 
 $xml = file_get_contents($url);
-file_put_contents($xml_file, $xml);
-#$xml = file_get_contents($xml_file);
-
+error_log('Descărcat ' . strlen($xml) . ' bytes');
 
 $array = array();
 $simple_xml = simplexml_load_string($xml);
@@ -30,8 +30,9 @@ foreach ($simple_xml->Valute as $valuta) {
   );
 };
 
-file_put_contents($json_file, 'var RateBNM = ' . json_encode($array));
+file_put_contents($js, 'var RateBNM = ' . json_encode($array) . ';');
+error_log("Înregistrat JS: $js " . filesize($js) . " bytes");
 
 $current = "$root/current.js";
 unlink($current);
-symlink($json_file, $current);
+symlink($js, $current);
