caa194acee6bd2153a669634c56a1e86b02c128b
Vlad GURDIGA <gurdiga@gmail.com>
2013-01-26 08:25:34 +0200

    build: testez concept nou

--- build/configurez-nginx.sh
+++ build/configurez-nginx.sh
@@ -1,6 +1,7 @@
 echo 'Configurez nginx...'
 
 SERVER_NAME=${SERVER_NAME-$(basename `pwd`)}
+echo $SERVER_NAME
 
 cp nginx.vhost.conf.template nginx.vhost.conf
 sed -i "s/%%SERVER_NAME%%/$SERVER_NAME/g" nginx.vhost.conf
--- build/stage.sh
+++ build/stage.sh
@@ -7,19 +7,19 @@ sudo mkdir -p $ROOT
 
 cd $ROOT
 sudo chown `whoami`:`whoami` . 
-mkdir -p data
-mkdir -p bnm #TODO: de pus datele de la BNM similar cu date
-mkdir -p releases
 
+mkdir -p data
+git clone ssh://git@bitbucket.org/gurdiga/data.executori.git --depth 0 data
+
+mkdir -p releases
 RELEASE=releases/`date +'%Y%m%d%H%M%S'`
 git clone ssh://git@bitbucket.org/gurdiga/executori.git --depth 0 $RELEASE
 
 ln -s $RELEASE stage
 cd $RELEASE
 
-SERVER_NAME="executori.org" make build
-
-# TODO: De curăţat /css şi /js înainte de concatenare
+export SERVER_NAME="executori.org"
+make build
 
 # TODO
 # ln -s /mnt/flash/date/data.executori.org $STAGE/date
