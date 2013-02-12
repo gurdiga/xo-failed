8f6bccfd401938f94bfd695eb6ce37ddcf423072
Vlad GURDIGA <gurdiga@gmail.com>
2013-02-12 18:41:43 +0200

    bibi

--- js/action.js
+++ js/action.js
@@ -761,14 +761,14 @@
           var $input = $(this),
               $label = $input.prev();
 
+          if (!$label.val() && !$input.val()) return;
+          if ($input.is('.dată.amînare')) return; // avem colecteazăAmînări() special pentru asta
+
           if ($label.is('.etichetă')) {
-            if (!$label.val() && !$input.val()) return;
             if (!date.subformular) date.subformular = {};
 
             date.subformular[$label.val()] = $input.is('.sumă') ? $input.suma() : $input.val();
           } else {
-            if ($input.is('.dată.amînare')) return; // avem colecteazăAmînări() special pentru asta
-
             date[$input.attr('id')] = $input.val1();
           }
         });
