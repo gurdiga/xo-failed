f3c14e69cea030d311a0695c0023b7f94f181ae9
Vlad GURDIGA <gurdiga@gmail.com>
2013-02-16 18:38:02 +0200

    bibi

--- build/teste/qunit/integrare/iniţializare-formular.js
+++ build/teste/qunit/integrare/iniţializare-formular.js
@@ -4,9 +4,8 @@ asyncTest('Procedură: iniţializare formular', function () {
   var app = this.app;
   var filăProcedurăDeOrdinGeneral = app.$('#crează-procedură li[data-href].g');
 
-  ok(
-    filăProcedurăDeOrdinGeneral.click()
-    .există(), 'găsit filă pentru crearea procedurii de ordin general');
+  ok(filăProcedurăDeOrdinGeneral.click(), 'găsit filă pentru crearea procedurii de ordin general');
+  filăProcedurăDeOrdinGeneral.există();
 
   app.FormularProcedură.$.one('iniţializat', function () {
     ok(true, 'iniţializat formularul');
