5685ac2fda3addacc3809d01064c9b52beaeb115
Vlad GURDIGA <gurdiga@gmail.com>
2013-03-04 23:12:03 +0200

    qunit: integrare: reparat test focusare în obiectul urmăririi

--- build/teste/qunit/integrare/verificare-obiectul-urmăririi.js
+++ build/teste/qunit/integrare/verificare-obiectul-urmăririi.js
@@ -5,9 +5,12 @@ test('Formular procedură: obiectul urmăririi', function () {
   var $secţiune = this.$obiectulUrmăririi,
       caracterIniţial = $secţiune.find('#caracter').val();
 
+  this.app.$.fx.off = true;
+
   equal($secţiune.find('#caracter').val(), 'pecuniar', 'Iniţial caracterul este “pecuniar”');
 
   $secţiune.find('#caracter').val('nonpecuniar').change();
+  equal($secţiune.find('#caracter').val(), 'nonpecuniar', 'setat caracterul nonpecuniar');
   ok($secţiune.find('select#obiect').există(), 'avem cîmp pentru pentru obiect');
   ok($secţiune.find('select#obiect+.buton[data-formular]').există(), 'avem butonaş pentru încheiere');
   equal($secţiune.find('select#obiect').val(), 'evacuarea', 'Iniţial obiectul este “evacuarea”');
@@ -28,11 +31,14 @@ test('Formular procedură: obiectul urmăririi', function () {
   $butonDeAdăugare.click();
   ok($secţiune.find('.etichetă.amînare').există(), 'la click pe butonul de adăugare se adaugă un cîmp pentru amînare');
   equal($secţiune.find('.etichetă.amînare').val(), 'Amînat pînă la', '…cu eticheta “Amînat pînă la”');
-  ok($secţiune.find('.etichetă.amînare').next('input').is(':focus'), '…cu cîmpul pentru dată focusat');
+  ok($secţiune.find('.etichetă.amînare').next('input').is(':focused'), '…cu cîmpul pentru dată focusat');
 
   ok($secţiune.find(':checkbox#se-acordă-alt-spaţiu').există(), 'avem bifă pentru “se acordă alt spaţiu”');
   ok($secţiune.find(':checkbox#se-acordă-alt-spaţiu').is(':not(:checked)'), '…nebifată');
 
   // restabileşte valoarea iniţială
   $secţiune.find('#caracter').val(caracterIniţial).change();
+  equal($secţiune.find('#caracter').val(), caracterIniţial, 'resetat caracterul iniţial');
+
+  this.app.$.fx.off = false;
 });
