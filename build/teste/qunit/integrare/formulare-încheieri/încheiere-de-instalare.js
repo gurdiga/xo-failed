c828df1af5ad0d4264f0f79a05ca08422bf04ae5
Vlad GURDIGA <gurdiga@gmail.com>
2013-03-06 17:13:20 +0200

    Încheiere cu privire la instalarea în spaţiul locativ: teste colectare/populare

--- build/teste/qunit/integrare/formulare-încheieri/încheiere-de-instalare.js
+++ build/teste/qunit/integrare/formulare-încheieri/încheiere-de-instalare.js
@@ -1,27 +1,35 @@
 // formularul de procedură trebuie se fi rămas deschis de la încheierile-referitoare-la-obiectul-urmăririi.js
 asyncTest('Încheiere de instalare', function () {
   /*global UtilitareÎncheiere:false */
-  /*jshint maxlen:129 */
+  /*jshint maxlen:136 */
   'use strict';
 
   var app = this.app,
+      $formular = app.FormularProcedură.$,
       $secţiune = app.FormularProcedură.$obiectulUrmăririi,
       obiect = 'instalarea';
 
-  ok(app.FormularProcedură.$.is(':visible'), 'formularul de procedură e deschis');
+  ok($formular.is(':visible'), 'formularul de procedură e deschis');
   $secţiune.find('#caracter').val('nonpecuniar').change();
   $secţiune.find('#obiect').val(obiect).change();
   equal($secţiune.find('#obiect').val(), obiect, 'setat obiectul corespunzător');
 
-  ok($secţiune.find('#în-încăperea').există(), 'avem cîmpul “În încăperea”');
+  var $dataŞiOraInstalării = $secţiune.find('#data-şi-ora-instalării');
 
-  var adresa = 'str. Cutărescu 17\nChişinău';
+  ok($dataŞiOraInstalării.există(), 'avem cîmp pentru data şi ora instalării');
+  equal($dataŞiOraInstalării.val(), '', '…implicit necompletat');
+  ok($secţiune.find('#în-încăperea').există(), 'avem cîmpul “În încăperea”');
+  equal($secţiune.find('#în-încăperea').val(), '', '…implicit necompletat');
+  ok(!$secţiune.find('.amînare.personalizat').există(), 'implicit nu sunt cîmpuri pentru amînări');
+
+  var adresa = 'str. Cutărescu 17\nChişinău',
+      dataŞiOraInstalării = '04.04.2014 ora 14:30';
 
   $secţiune.find('#în-încăperea').val(adresa);
+  $dataŞiOraInstalării.val(dataŞiOraInstalării);
 
   // adăugăm o amînare
-  var $dataEvacuării = $secţiune.find('li:has(#data-şi-ora-instalării)'),
-      $container = $dataEvacuării.next('.container-buton'),
+  var $container = $dataŞiOraInstalării.parent().next('.container-buton'),
       $butonDeAdăugare = $container.find('button.adaugă-cîmp-personalizat');
 
   ok($butonDeAdăugare.există(), 'avem buton de adăugare amînare');
@@ -33,37 +41,67 @@ asyncTest('Încheiere de instalare', function () {
   ok($amînare.există(), 's-a adăugat cîmp pentru amînare');
   $amînare.find('.dată').val(dataAmînării);
 
-  var $butonPentruÎncheiere = $secţiune.find('#obiect~.buton[data-formular]');
 
-  ok($butonPentruÎncheiere.există(), 'găsit butonaşul pentru încheiere');
-  $butonPentruÎncheiere.click();
+  var procedură = app.FormularProcedură.colectează();
 
-  var formular = app.ButoanePentruÎncheieri.formular($butonPentruÎncheiere),
-      meta = app.Încheieri.deschise[formular];
+  (function verificăColectarea() {
+    equal(procedură['obiectul-urmăririi']['data-şi-ora-instalării'], dataŞiOraInstalării, 'colectarea: data şi ora instalării');
+    equal(procedură['obiectul-urmăririi']['în-încăperea'], adresa, 'colectarea: adresa');
+    equal(procedură['obiectul-urmăririi']['amînări'].length, 1, 'colectarea: numărul amînărilor corespunde');
+    equal(procedură['obiectul-urmăririi']['amînări'][0]['Amînat pînă la'], dataAmînării, 'colectarea: data şi ora amînării corespunde');
+  })();
 
-  app.$(meta).one('iniţializat', function () {
-    var $încheiere = app.$(this.tab.document),
-        date = this.tab.Încheiere.date;
 
-    ok(date.amînată, 'context: setat amînată');
-    equal(date.ultimaAmînare, dataAmînării, 'context: setat ultimaAmînare');
+  $formular.find('.bara-de-instrumente .salvează').click();
+  $formular.one('salvat', function () {
+    ok(true, 'salvat');
+    $formular.one('închis', function () {
+      app.ProceduriRecente.$.find('.item:first').click();
+      $formular.one('populat', function () {
+        ok(true, 'redeschis şi populat');
 
-    var $paragrafDespreAmînare = $încheiere.find('p:contains("a fost amînată pînă la")');
+        equal($secţiune.find('#în-încăperea').val(), adresa, 'popularea: adresa');
+        equal($secţiune.find('#data-şi-ora-instalării').val(), dataŞiOraInstalării, 'popularea: data şi ora instalării');
+        equal($secţiune.find('.amînare.personalizat').length, 1, 'popularea: corespunde numărul de amînări');
+        equal($secţiune.find('.amînare.personalizat .etichetă').val(), 'Amînat pînă la', 'popularea: corespunde eticheta amînării');
+        equal($secţiune.find('.amînare.personalizat .dată').val(), dataAmînării, 'popularea: corespunde data şi ora amînării');
 
-    ok($paragrafDespreAmînare.există(), 'avem paragraf despre amînare');
-    ok($paragrafDespreAmînare.find('span.atenţionare').există(), '…cu atenţionare să se introducă cauza');
-    ok($încheiere.find('section header:contains("Motivele")+.conţinut').is('.editabil'), 'secţiunea “Motivele” este editabilă');
-    equal($încheiere.find('address').first().text().trim(), adresa, 'avem adresa');
+        var $butonPentruÎncheiere = $secţiune.find('#obiect~.buton[data-formular]');
 
-    UtilitareÎncheiere.verificăŞoaptăButon($încheiere, $butonPentruÎncheiere);
-    UtilitareÎncheiere.verificăSubtitlu($încheiere, 'cu privire la instalarea în spaţiul locativ');
-    UtilitareÎncheiere.verificăSecţiuni($încheiere,
-      ['Procedura', 'Creditorul', 'Debitorul', 'Chestiunea', 'Motivele', 'Dispoziţia', 'Executorul']);
+        ok($butonPentruÎncheiere.există(), 'găsit butonaşul pentru încheiere');
+        $butonPentruÎncheiere.click();
 
-    setTimeout(function () { // pentru observabilitate
-      $încheiere.find('.închide').click();
+        var formular = app.ButoanePentruÎncheieri.formular($butonPentruÎncheiere),
+            meta = app.Încheieri.deschise[formular];
 
-      start();
-    }, app.PAUZĂ_DE_OBSERVABILITATE);
-  });
+        app.$(meta).one('iniţializat', function () {
+          var $încheiere = app.$(this.tab.document),
+              date = this.tab.Încheiere.date;
+
+          ok(date.amînată, 'context: setat amînată');
+          equal(date.ultimaAmînare, dataAmînării, 'context: setat ultimaAmînare');
+
+          var $paragrafDespreAmînare = $încheiere.find('p:contains("a fost amînată pînă la")');
+
+          ok($paragrafDespreAmînare.există(), 'avem paragraf despre amînare');
+          ok($paragrafDespreAmînare.find('span.atenţionare').există(), '…cu atenţionare să se introducă cauza');
+          ok($încheiere.find('section header:contains("Motivele")+.conţinut').is('.editabil'), 'secţiunea “Motivele” este editabilă');
+          equal($încheiere.find('address').first().text().trim(), adresa, 'avem adresa');
+
+          UtilitareÎncheiere.verificăŞoaptăButon($încheiere, $butonPentruÎncheiere);
+          UtilitareÎncheiere.verificăSubtitlu($încheiere, 'cu privire la instalarea în spaţiul locativ');
+          UtilitareÎncheiere.verificăSecţiuni($încheiere,
+            ['Procedura', 'Creditorul', 'Debitorul', 'Chestiunea', 'Motivele', 'Dispoziţia', 'Executorul']);
+
+          setTimeout(function () { // pentru observabilitate
+            $încheiere.find('.închide').click();
+
+            start();
+          }, app.PAUZĂ_DE_OBSERVABILITATE);
+        });
+      }); // one populat
+    }); // one închis
+
+    $formular.find('button.închide').click();
+  }); // one salvat
 });
