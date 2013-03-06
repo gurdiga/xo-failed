db89db7d59688f2e15b96b533bb85a1face54cc3
Vlad GURDIGA <gurdiga@gmail.com>
2013-03-06 16:09:39 +0200

    încheiere-de-efectuare-a-unor-acţiuni-nelegate-de-bunuri: teste colectare/populare

--- build/teste/qunit/integrare/formulare-încheieri/încheiere-de-efectuare-a-unor-acţiuni-nelegate-de-bunuri.js
+++ build/teste/qunit/integrare/formulare-încheieri/încheiere-de-efectuare-a-unor-acţiuni-nelegate-de-bunuri.js
@@ -5,10 +5,11 @@ asyncTest('Somaţie cu privire la efectuarea unor acţiuni nelegate de remiterea
   'use strict';
 
   var app = this.app,
+      $formular = app.FormularProcedură.$,
       $secţiune = app.FormularProcedură.$obiectulUrmăririi,
       obiect = 'efectuarea de către debitor a unor acţiuni obligatorii, nelegate de remiterea unor sume sau bunuri';
 
-  ok(app.FormularProcedură.$.is(':visible'), 'formularul de procedură e deschis');
+  ok($formular.is(':visible'), 'formularul de procedură e deschis');
   $secţiune.find('#caracter').val('nonpecuniar').change();
   $secţiune.find('#obiect').val(obiect).change();
   equal($secţiune.find('#obiect').val(), obiect, 'setat obiectul corespunzător');
@@ -18,31 +19,53 @@ asyncTest('Somaţie cu privire la efectuarea unor acţiuni nelegate de remiterea
   ok($secţiune.find('#acţiuni').există(), 'avem cîmp pentru acţiuni');
   $secţiune.find('#acţiuni').val(acţiuni);
 
-  var $butonPentruÎncheiere = $secţiune.find('#obiect~.buton[data-formular]');
 
-  ok($butonPentruÎncheiere.există(), 'găsit butonaşul pentru încheiere');
-  $butonPentruÎncheiere.click();
+  var procedură = app.FormularProcedură.colectează(),
+      acţiuniColectate = procedură['obiectul-urmăririi']['acţiuni'];
 
-  var formular = app.ButoanePentruÎncheieri.formular($butonPentruÎncheiere),
-      meta = app.Încheieri.deschise[formular];
+  equal(acţiuniColectate, acţiuni, 'colectare: acţiunile corespund');
 
-  app.$(meta).one('iniţializat', function () {
-    var $încheiere = app.$(this.tab.document),
-        subtitlu = 'cu privire la executarea de către debitor a unor acţiuni nelegate de remiterea unor sume sau bunuri';
 
-    UtilitareÎncheiere.verificăŞoaptăButon($încheiere, $butonPentruÎncheiere);
-    UtilitareÎncheiere.verificăSubtitlu($încheiere, subtitlu);
-    UtilitareÎncheiere.verificăSecţiuni($încheiere, ['Procedura', 'Creditorul', 'Debitorul', 'Executorul']);
+  $formular.find('.bara-de-instrumente .salvează').click();
+  $formular.one('salvat', function () {
+    ok(true, 'salvat');
+    $formular.one('închis', function () {
+      app.ProceduriRecente.$.find('.item:first').click();
+      $formular.one('populat', function () {
+        ok(true, 'redeschis şi populat');
 
-    var $conţinut = $încheiere.find('section .conţinut.pe-toată-foaia');
+        equal($secţiune.find('#acţiuni').val(), acţiuni, 'populare: acţiunile se populează corespunzător');
 
-    ok($conţinut.există(), 'avem secţiunea atotcuprinzătoare');
-    equal($conţinut.find('.acţiuni').text(), acţiuni, 'se menţionează acţiunile');
+        var $butonPentruÎncheiere = $secţiune.find('#obiect~.buton[data-formular]');
 
-    setTimeout(function () {
-      $încheiere.find('.închide').click();
+        ok($butonPentruÎncheiere.există(), 'găsit butonaşul pentru încheiere');
+        $butonPentruÎncheiere.click();
 
-      start();
-    }, app.PAUZĂ_DE_OBSERVABILITATE);
-  });
+        var formular = app.ButoanePentruÎncheieri.formular($butonPentruÎncheiere),
+            meta = app.Încheieri.deschise[formular];
+
+        app.$(meta).one('iniţializat', function () {
+          var $încheiere = app.$(this.tab.document),
+              subtitlu = 'cu privire la executarea de către debitor a unor acţiuni nelegate de remiterea unor sume sau bunuri';
+
+          UtilitareÎncheiere.verificăŞoaptăButon($încheiere, $butonPentruÎncheiere);
+          UtilitareÎncheiere.verificăSubtitlu($încheiere, subtitlu);
+          UtilitareÎncheiere.verificăSecţiuni($încheiere, ['Procedura', 'Creditorul', 'Debitorul', 'Executorul']);
+
+          var $conţinut = $încheiere.find('section .conţinut.pe-toată-foaia');
+
+          ok($conţinut.există(), 'avem secţiunea atotcuprinzătoare');
+          equal($conţinut.find('.acţiuni').text(), acţiuni, 'se menţionează acţiunile');
+
+          setTimeout(function () {
+            $încheiere.find('.închide').click();
+
+            start();
+          }, app.PAUZĂ_DE_OBSERVABILITATE);
+        });
+      }); // one populat
+    }); // one închis
+
+    $formular.find('button.închide').click();
+  }); // one salvat
 });
