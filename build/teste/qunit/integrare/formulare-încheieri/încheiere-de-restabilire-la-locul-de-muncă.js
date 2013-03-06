450ae720c1586a8fcd85545dc50ba4fd6649f38e
Vlad GURDIGA <gurdiga@gmail.com>
2013-03-06 17:59:08 +0200

    Somaţie de restabilire a salariatului la locul de muncă: teste colectare/populare

--- build/teste/qunit/integrare/formulare-încheieri/încheiere-de-restabilire-la-locul-de-muncă.js
+++ build/teste/qunit/integrare/formulare-încheieri/încheiere-de-restabilire-la-locul-de-muncă.js
@@ -5,45 +5,77 @@ asyncTest('Somaţie de restabilire a salariatului la locul de muncă', function 
   'use strict';
 
   var app = this.app,
+      $formular = app.FormularProcedură.$,
       $secţiune = app.FormularProcedură.$obiectulUrmăririi,
       obiect = 'restabilirea la locul de muncă';
 
-  ok(app.FormularProcedură.$.is(':visible'), 'formularul de procedură e deschis');
+  ok($formular.is(':visible'), 'formularul de procedură e deschis');
   $secţiune.find('#caracter').val('nonpecuniar').change();
   $secţiune.find('#obiect').val(obiect).change();
   equal($secţiune.find('#obiect').val(), obiect, 'setat obiectul corespunzător');
 
-  var funcţie = 'pilot de curse';
+  var funcţie = 'pilot de curse',
+      salariuRecuperat = '12000';
 
   ok($secţiune.find('#funcţie').există(), 'avem cîmp pentru funcţie');
+  equal($secţiune.find('#funcţie').val(), '', '…implicit necompletat');
   $secţiune.find('#funcţie').val(funcţie);
+  ok($secţiune.find('#salariu-recuperat').există(), 'avem cîmp pentru suma salariului recuperat');
+  equal($secţiune.find('#salariu-recuperat').val(), '', '…implicit necompletat');
+  $secţiune.find('#salariu-recuperat').val(salariuRecuperat);
 
-  var $butonPentruÎncheiere = $secţiune.find('#obiect~.buton[data-formular]');
 
-  ok($butonPentruÎncheiere.există(), 'găsit butonaşul pentru încheiere');
-  $butonPentruÎncheiere.click();
+  var procedură = app.FormularProcedură.colectează();
 
-  var formular = app.ButoanePentruÎncheieri.formular($butonPentruÎncheiere),
-      meta = app.Încheieri.deschise[formular];
+  (function verificăColectarea() {
+    equal(procedură['obiectul-urmăririi']['funcţie'], funcţie, 'colectarea: funcţia');
+    equal(procedură['obiectul-urmăririi']['salariu-recuperat'], salariuRecuperat, 'colectarea: salariul recuperat');
+  })();
 
-  app.$(meta).one('iniţializat', function () {
-    var $încheiere = app.$(this.tab.document),
-        subtitlu = 'de restabilire a salariatului la locul de muncă';
 
-    UtilitareÎncheiere.verificăŞoaptăButon($încheiere, $butonPentruÎncheiere);
-    UtilitareÎncheiere.verificăSubtitlu($încheiere, subtitlu);
-    UtilitareÎncheiere.verificăSecţiuni($încheiere, ['Procedura', 'Creditorul', 'Debitorul', 'Executorul']);
+  $formular.find('.bara-de-instrumente .salvează').click();
+  $formular.one('salvat', function () {
+    ok(true, 'salvat');
+    $formular.one('închis', function () {
+      app.ProceduriRecente.$.find('.item:first').click();
+      $formular.one('populat', function () {
+        ok(true, 'redeschis şi populat');
 
-    var $conţinut = $încheiere.find('section .conţinut.pe-toată-foaia');
+        equal($secţiune.find('#funcţie').val(), funcţie, 'popularea: funcţie');
+        equal($secţiune.find('#salariu-recuperat').val(), salariuRecuperat, 'popularea: salariul lrecuperat');
 
-    ok($conţinut.există(), 'avem secţiunea atotcuprinzătoare');
-    equal($conţinut.find('p:contains("' + funcţie + '")').length, 2, 'se menţionează funcţia în două rînduri');
 
-    setTimeout(function () {
-      $încheiere.find('.închide').click();
+        var $butonPentruÎncheiere = $secţiune.find('#obiect~.buton[data-formular]');
 
-      start();
-    }, app.PAUZĂ_DE_OBSERVABILITATE);
-  });
+        ok($butonPentruÎncheiere.există(), 'găsit butonaşul pentru încheiere');
+        $butonPentruÎncheiere.click();
+
+        var formular = app.ButoanePentruÎncheieri.formular($butonPentruÎncheiere),
+            meta = app.Încheieri.deschise[formular];
+
+        app.$(meta).one('iniţializat', function () {
+          var $încheiere = app.$(this.tab.document),
+              subtitlu = 'de restabilire a salariatului la locul de muncă';
+
+          UtilitareÎncheiere.verificăŞoaptăButon($încheiere, $butonPentruÎncheiere);
+          UtilitareÎncheiere.verificăSubtitlu($încheiere, subtitlu);
+          UtilitareÎncheiere.verificăSecţiuni($încheiere, ['Procedura', 'Creditorul', 'Debitorul', 'Executorul']);
+
+          var $conţinut = $încheiere.find('section .conţinut.pe-toată-foaia');
+
+          ok($conţinut.există(), 'avem secţiunea atotcuprinzătoare');
+          equal($conţinut.find('p:contains("' + funcţie + '")').length, 2, 'se menţionează funcţia în două rînduri');
+
+          setTimeout(function () {
+            $încheiere.find('.închide').click();
+
+            start();
+          }, app.PAUZĂ_DE_OBSERVABILITATE);
+        });
+      }); // one populat
+    }); // one închis
+
+    $formular.find('button.închide').click();
+  }); // one salvat
 });
 
