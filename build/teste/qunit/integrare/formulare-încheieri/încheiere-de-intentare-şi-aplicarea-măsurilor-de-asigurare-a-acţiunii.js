143a7127f199c9cac29f071a15abe8b1dbf0406b
Vlad GURDIGA <gurdiga@gmail.com>
2013-03-31 21:23:39 +0300

    eliminat testele pentru variaţiile clauzelor în încheierea de intentare şi asigurare a măsurilor (prea complicat)

--- build/teste/qunit/integrare/formulare-încheieri/încheiere-de-intentare-şi-aplicarea-măsurilor-de-asigurare-a-acţiunii.js
+++ build/teste/qunit/integrare/formulare-încheieri/încheiere-de-intentare-şi-aplicarea-măsurilor-de-asigurare-a-acţiunii.js
@@ -53,63 +53,10 @@ asyncTest('Încheiere de intentare a procedurii de executare şi de asigurare a 
           ok(!$secţiuneaDispoziţia.find('.clauze-referitoare-la' + clauză).există(), 'iniţial nu se afişează clauzele referitoare la ' + descriereClauză);
         }
 
-        app.$(încheiere).one('închis', function () {
-          verificăPrezenţa(clauze);
-        });
-
         $încheiere.find('.închide').click();
+        start();
       });
     });
   });
-
-
-  function verificăPrezenţa(clauze) {
-    equal(Object.keys(app.Încheieri.deschise).length, 0, 'nu sunt încheieri deschise');
-
-    var itemi = Object.keys(clauze),
-        clauză = itemi.shift(),
-        descriereClauză = clauze[clauză];
-
-    $formular.find('#categorii-taxe-şi-speze #taxaB1').click();
-
-    var $taxaB1 = app.Cheltuieli.$.find('.adăugate #taxaB1');
-
-    ok($taxaB1.există(), 'adăugat taxaB1');
-    $taxaB1.find('.document:first .adaugă-destinatar').trigger('mouseenter');
-    app.Destinatari.$.find(clauză).click();
-    ok($taxaB1.find('.document:first .destinatari-adăugaţi ' + clauză).există(), 'adăugat ' + descriereClauză);
-
-    $buton.click();
-
-    app.$(app.Încheieri.deschise[formular]).one('iniţializat', function (e, window) {
-      var încheiere = window,
-          date = încheiere.Încheiere.date,
-          $încheiere = app.$(încheiere.document.body),
-          $secţiuneaDispoziţia = $încheiere.find('section header:contains("Dispoziţia")+.conţinut');
-
-      ok(date.afişeazăClauza[clauză], 'context: setat flag de afişare pentru ' + descriereClauză);
-      $încheiere = app.$(încheiere.document.body);
-
-      ok($secţiuneaDispoziţia.find('.clauze-referitoare-la' + clauză).există(), 'găsit clauzele referitoare la ' + descriereClauză);
-
-      setTimeout(function () { // pentru observabilitate
-        app.$(încheiere).one('închis', function () {
-          equal(Object.keys(app.Încheieri.deschise).length, 0, 'nu au rămas încheieri deschise înregistrate');
-          app.Cheltuieli.$.find('.adăugate #taxaB1').remove();
-
-          delete clauze[clauză];
-
-          if ($.isEmptyObject(clauze)) {
-            app.$.fx.off = false;
-            start();
-          } else {
-            verificăPrezenţa(clauze);
-          }
-        });
-
-        $încheiere.find('.închide').click();
-      }, app.PAUZĂ_DE_OBSERVABILITATE);
-    });
-  }
 
 });
