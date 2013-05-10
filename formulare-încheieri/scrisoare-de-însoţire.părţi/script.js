fb1c3d34ab8afc96fd118729a418b96e6953ace4
Vlad GURDIGA <gurdiga@gmail.com>
2013-05-11 00:48:20 +0300

    formează fiţuici doar pentru destinatarii necesari

--- formulare-încheieri/scrisoare-de-însoţire.părţi/script.js
+++ formulare-încheieri/scrisoare-de-însoţire.părţi/script.js
@@ -1,22 +1,39 @@
 window.init = function (context) {
+  /*jshint maxcomplexity:7*/
   'use strict';
 
-  context.debitori = context.procedură['debitori'];
-  context.persoaneTerţe = context.procedură['persoane-terţe'];
+  var destinatari = context.buton.attr('destinatari').split(' '),
+      pentru = {};
+
+  for (var k = 0; k < destinatari.length; k++) {
+    pentru[destinatari[k]] = true;
+  }
+
+  context.pentru = pentru;
+
+  context.debitori = pentru['debitori'] ? context.procedură['debitori'] : [];
+  context.persoaneTerţe = pentru['persoane-terţe'] ? context.procedură['persoane-terţe'] : [];
   context.alţiDestinatari = [];
 
-  if ('taxaB1' in context.procedură['cheltuieli'].itemi) {
-    var documenteExpediate = context.procedură['cheltuieli'].itemi['taxaB1'].subformular,
-        destinatari, i, j;
+  if (pentru['alţi-destinatari']) {
+    if ('taxaB1' in context.procedură['cheltuieli'].itemi) {
+      var documenteExpediate = context.procedură['cheltuieli'].itemi['taxaB1'].subformular,
+          colecţie, i, j;
 
-    context.alţiDestinatari = [];
+      for (i = 0; i < documenteExpediate.length; i++) {
+        colecţie = documenteExpediate[i]['destinatari'];
 
-    for (i = 0; i < documenteExpediate.length; i++) {
-      context.alţiDestinatari = context.alţiDestinatari.concat(
-        documenteExpediate[i]['destinatari'],
-        documenteExpediate[i]['destinatari-persoane-terţe']
-      );
+        for (j = 0; j < colecţie.length; j++) {
+          if (colecţie[j] === 'Creditorul') continue;
+          if (colecţie[j] === 'Debitorul') continue;
+
+          context.alţiDestinatari.push(colecţie[j].replace(/ (\S+),/, '<br/>$1,'));
+        }
+
+        context.alţiDestinatari = context.alţiDestinatari.concat(
+          documenteExpediate[i]['destinatari-persoane-terţe']
+        );
+      }
     }
   }
-  console.log(context.alţiDestinatari);
 };
