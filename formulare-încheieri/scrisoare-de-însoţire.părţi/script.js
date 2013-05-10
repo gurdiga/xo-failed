4dc6f1f142eff4f71ddd2c89c1d205b96ec874d4
Vlad GURDIGA <gurdiga@gmail.com>
2013-05-10 23:49:43 +0300

    scrisoare de însoţire pentru persoane terţe, bănci şi alte instituţii

--- formulare-încheieri/scrisoare-de-însoţire.părţi/script.js
+++ formulare-încheieri/scrisoare-de-însoţire.părţi/script.js
@@ -1,1 +1,22 @@
-// nothing inhere yet
+window.init = function (context) {
+  'use strict';
+
+  context.debitori = context.procedură['debitori'];
+  context.persoaneTerţe = context.procedură['persoane-terţe'];
+  context.alţiDestinatari = [];
+
+  if ('taxaB1' in context.procedură['cheltuieli'].itemi) {
+    var documenteExpediate = context.procedură['cheltuieli'].itemi['taxaB1'].subformular,
+        destinatari, i, j;
+
+    context.alţiDestinatari = [];
+
+    for (i = 0; i < documenteExpediate.length; i++) {
+      context.alţiDestinatari = context.alţiDestinatari.concat(
+        documenteExpediate[i]['destinatari'],
+        documenteExpediate[i]['destinatari-persoane-terţe']
+      );
+    }
+  }
+  console.log(context.alţiDestinatari);
+};
--- formulare-încheieri/încheiere-de-continuare.părţi/script.js
+++ formulare-încheieri/încheiere-de-continuare.părţi/script.js
@@ -1,3 +1,4 @@
+// nothing inhere yet
 window.init = function (context) {
   'use strict';
 
