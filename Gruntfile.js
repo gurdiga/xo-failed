6dde3167a82cd2a65fea406c649c89308e5d737e
Vlad GURDIGA <gurdiga@gmail.com>
2014-01-09 07:08:12 +0200

    grunt-html-build

--- Gruntfile.js
+++ Gruntfile.js
@@ -76,6 +76,39 @@
           dest: '<%= gorilla.compile.cwd %>',
           ext: '.compiled.js' // enable sourcemaps based on extension?
         }
+      },
+
+      htmlbuild: {
+        index: {
+          src: 'index.template.html',
+          dest: './index.html',
+          options: {
+            relative: false,
+            scripts: {
+              lib: [
+                'js/lib/jquery*.js',
+                'lib/jquery-ui-1.9.2.custom/js/jquery-ui-1.9.2.custom.min.js',
+                'js/lib.js',
+                'js/lib/firebase.js',
+                'js/lib/firebase*.js',
+                'js/lib/angular.min.js',
+                'js/lib/angular*.js'
+              ],
+              app: [
+                'js/app/init.js',
+                'js/app/**/*.js',
+                'js/action.js'
+              ],
+            },
+            sections: {
+              templates: 'js/app/**/*.html'
+            }
+          }
+        },
+
+        qunit: {
+          // TODO
+        }
       }
     });
 
@@ -83,10 +116,13 @@
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-html-build');
     grunt.loadNpmTasks('grunt-gorilla');
 
+    grunt.registerTask('html', ['htmlbuild']);
     grunt.registerTask('default', ['jshint']);
     grunt.registerTask('pre-commit', ['jshint']);
+
 
   };
 })();
