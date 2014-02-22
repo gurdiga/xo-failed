f58c3ed4dfdcf42230c033c6acfa2a86ee4ffedc
Vlad GURDIGA <gurdiga@gmail.com>
2014-02-22 19:17:14 +0200

    Grunt subset-tests task

--- tasks/test.js
+++ tasks/test.js
@@ -2,5 +2,14 @@
 
 module.exports = function(grunt) {
   grunt.registerTask('test', ['html', 'lint', 'mocha_phantomjs']);
+
+  grunt.registerTask('subset-tests', 'Only run the specified suset of tests', function(subset) {
+    if (!subset) {
+      grunt.log.error('This task needs the subset specified like this: subset-tests:test/util/*.js');
+      return false;
+    }
+
+    grunt.config('tests', subset.split(','));
+  });
 };
 
